8238f9ecbdbc7cf61543e8167d8e54807e29f968
rmammina <rmammina@gmail.com>
2015-10-08 09:08:14 -0700

    food entry controller rest methods added.

--- assets/controllers.js
+++ assets/controllers.js
@@ -3,6 +3,7 @@ angular.module('webtools.controllers', [])
 angular.module('webtools.controllers').controller('FoodEntryCtrl', function($scope, FoodEntryModel) {
 	$scope.entries = []
 
+	// Load all the food entries.
 	$scope.load = function() {
 		FoodEntryModel.getAll().then(function() {
 
@@ -10,6 +11,33 @@ angular.module('webtools.controllers').controller('FoodEntryCtrl', function($sco
 
 		});
 
+	}
+
+	// Load a single food entry by id.
+	$scope.get = function(entryId) {
+		FoodEntryModel.get(entryId).then(function() {
+
+		}, function() {
+
+		})
+	}
+
+	// Update a food entry.
+	$scope.update = function(entryId) {
+		FoodEntryModel.update(entryId).then(function() {
+
+		}, function() {
+
+		})
+	}
+
+	// Delete a food entry.
+	$scope.delete = function(entryId) {
+		FoodEntryModel.delete(entryId).then(function() {
+
+		}, function() {
+
+		})
 	}
 
 })
--- assets/service.js
+++ assets/service.js
@@ -1,5 +1,36 @@
 angular.module('webtools.service', []);
 
-angular.module('webtools.service').service('FoodEntryModel', function() {
+angular.module('webtools.service').service('FoodEntryModel', function($q) {
 
+
+	this.getAll = function() {
+		var deferred = $q.defer();
+
+		deferred.resolve()
+		return deferred.promise() 
+	}
+
+	this.get = function(id) {
+		var deferred = $q.defer();
+
+		deferred.resolve()
+		return deferred.promise() 
+	}
+
+	this.update = function(id) {
+		var deferred = $q.defer();
+
+		deferred.resolve()
+		return deferred.promise() 
+	}
+
+	this.delete = function(id) {
+		var deferred = $q.defer();
+		
+		deferred.resolve()
+		return deferred.promise() 
+	}
+
+
+	return this;
 })
